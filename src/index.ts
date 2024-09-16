be5396243f4980d5d5206f6788c3720381026611
enderdev-v <endercrackgamer@outlook.com>
2024-09-15 18:55:11 -0600

    Solve host bugs

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Logger } from "seyfert";
 import { enderbot } from "#enderbot/client";
 import { customLog } from "#enderbot/classes/Logger.js";
 import "dotenv/config";
+import "#enderbot/utils/anticrash.js"
+import "./server"
 
 Logger.customize(customLog);
 Logger.saveOnFile = "all";
@@ -9,5 +11,3 @@ Logger.dirname = "logs";
 
 export const client = new enderbot();
 client.run()
-
-import "#enderbot/utils/anticrash.js"
