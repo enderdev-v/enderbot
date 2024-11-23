53cf003a710d5414222ee00072e9acda2e54946c
enderdev-v <endercrackgamer@outlook.com>
2024-11-23 12:53:17 -0600

    Solve some bug

--- src/events/bot/miniAI.ts
+++ src/events/bot/miniAI.ts
@@ -1,14 +1,16 @@
 import { createEvent } from "seyfert";
+import ms from "ms"
 export default createEvent({
     data: { name: "messageCreate" },
     async run(message) {
 
         if (message.author.bot) return;
 
-        
-        if (message.content.includes(("hola").toLowerCase()) || message.content.includes(("ola").toLowerCase())) {
-            message.reply({ content: `hola ${message.author} musho gusto` });
-        }
+        setTimeout(() => {
+            if (message.content.includes(("hola").toLowerCase()) || message.content.includes(("ola").toLowerCase())) {
+                message.reply({ content: `hola ${message.author} musho gusto` });
+            }
+        }, ms("2d"))
         if (!message.content.startsWith("enderbot ")) return;
         if (message.content.includes(("amigos").toLowerCase())) {
             message.reply({ content: `hola mis amigos son ThemurftBot y endkachu :D` });
